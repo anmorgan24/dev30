be4d71f8fbb0eb41dfc60279caf3ff6a4b487a90
Abigail Morgan <86856445+anmorgan24@users.noreply.github.com>
2022-02-21 18:37:11 -0500

    Create truthy_and_falsey.js

---
+++ truthy_and_falsey.js
@@ -0,0 +1,15 @@
+if ('hi') { console.log("truthy") } else { console.log("falsey") }; // truthy
+
+if (null) { console.log("truthy") } else { console.log("falsey") }; // falsey
+
+if (undefined) {console.log("truthy") } else { console.log("falsey") }; // falsey
+
+if (0) {console.log("truthy") } else { console.log("falsey") }; // falsey
+
+if ('') {console.log("truthy") } else { console.log("falsey") }; // falsey
+
+if (865) { console.log("truthy") } else { console.log("falsey") }; // truthy
+
+if (-9.7) {console.log("truthy") } else { console.log("falsey") }; // truthy
+
+if (NaN) {console.log("truthy") } else {console.log("falsey") }; // falsey
