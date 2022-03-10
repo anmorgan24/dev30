b9ede85fd7ad65d5a1a2018579f912795c03c34d
Abigail Morgan <anmorgan24@gmail.com>
2022-03-09 21:36:38 -0500

    whaleTalk.js project

---
+++ whaleTalk.js
@@ -0,0 +1,34 @@
+// Whale Talk
+
+// There are a few simple rules for translating text to whale language:
+    // 1. There are no consonants. Only vowels excluding “y”.
+    // 2. The u‘s and e‘s are extra long, so we must double them in our program.
+
+// var input = "There is, one knows not what sweet mystery about this sea,\
+//  whose gently awful stirrings seem to speak of some hidden soul beneath";
+
+var input = window.prompt("What would you like to translate to whale talk?");
+
+const vowels = ["a", "e", "i", "o", "u"];
+
+var resultArray = []
+
+for (let i = 0; i < input.length; i++) {
+    // console.log(char);
+    // var element = input[char];
+    let char = input[i]
+    for (let j = 0; j < vowels.length; j++) {
+        // const element = array[index];
+        // console.log(vowel);
+        let vowel = vowels[j]
+        if (vowel === char){
+            if (vowel === 'e' || vowel === 'u') {
+                resultArray.push(vowel, vowel);
+            }
+            else {
+                resultArray.push(vowel);
+            }
+        }
+    }  
+};  
+console.log(resultArray.join('').toUpperCase());
